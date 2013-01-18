1930b749cc8db6ab53fb8b2d009b0be763926fa9
qishang <1434609752@qq.com>
2013-01-18 11:13:14 +0800

    Initial Commit

---
+++ Entrance_Grab_English/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Entrance_Grab_English
+//
+//  Created by ibokan on 13-1-18.
+//  Copyright (c) 2013年 天朝码农. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Entrance_Grab_English/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Entrance_Grab_English
+//
+//  Created by ibokan on 13-1-18.
+//  Copyright (c) 2013年 天朝码农. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
